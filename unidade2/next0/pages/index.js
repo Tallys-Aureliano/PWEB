0f6b4f9b3f09f6bd52d05f2cafab19177f1eb2e8
Tallys-Aureliano <tallysaureliano@outlook.com>
2023-10-30 11:44:23 -0300

    :sparkles:Feat: Calling components

--- unidade2/next0/pages/index.js
+++ unidade2/next0/pages/index.js
@@ -1,9 +1,12 @@
-export default function Home() {
+import { home } from "./modules/home";
+import { login } from "./modules/login";
+export default function index() {
+
     return (
         <div>
-            <h2>
-                Viva Santana!
-            </h2>
+            <h1>Bem vindo ao Next</h1>
+            {home()}
+            {login()}
         </div>
     )
 }
