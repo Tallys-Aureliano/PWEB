8cd34cb2821721995788b23cf8085ef5b73f6350
Tallys-Aureliano <tallysaureliano@outlook.com>
2023-10-30 11:43:18 -0300

    :sparkles:Feat: Components for home

---
+++ unidade2/next0/pages/modules/login.js
@@ -0,0 +1,18 @@
+export function login() {
+    return (
+        <div>
+            <h2>Componente Login</h2>
+            <forms>
+                <label>
+                    Email
+                    <input type="email" />
+                </label>
+                <label>
+                    Senha
+                    <input type="password" />
+                </label>
+                <button type="submit">Entrar</button>
+            </forms>
+        </div>
+    )
+}
