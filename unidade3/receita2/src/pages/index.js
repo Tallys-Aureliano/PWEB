15352dcedb7512c1c21b59fac4fe3c52781a0e62
Tallys-Aureliano <tallysaureliano@outlook.com>
2023-11-15 19:08:15 -0300

    :sparkles:feat: calling components

--- unidade3/receita2/src/pages/index.js
+++ unidade3/receita2/src/pages/index.js
@@ -1,12 +1,13 @@
 import { home } from "./components/home";
 import { login } from "./components/login";
+import { navaBar } from "./components/navaBar";
 export default function index() {
 
     return (
-        <div>
-            <h1>Bem vindo ao Next</h1>
+        <section>
+            {navaBar()}
             {home()}
             {login()}
-        </div>
+        </section>    
     )
 }
