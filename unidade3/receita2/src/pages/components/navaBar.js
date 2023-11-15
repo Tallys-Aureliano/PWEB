becd4b0f5c04d0fb643ae561ef5426ec18f3cade
Tallys-Aureliano <tallysaureliano@outlook.com>
2023-11-15 19:07:13 -0300

    :sparkles:feat: navbar component

---
+++ unidade3/receita2/src/pages/components/navaBar.js
@@ -0,0 +1,11 @@
+export function navaBar() {
+    return (
+        <nav>
+            <h1>Receita 2</h1>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/">Login</a></li>
+            </ul>
+        </nav>
+    )
+}
