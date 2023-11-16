c820e50ea1fbe0673badc07347a72a021d788575
Tallys-Aureliano <tallysaureliano@outlook.com>
2023-11-15 21:32:45 -0300

    :sparkles:feat: adding navbar parameters

--- unidade3/receita2/src/pages/index.js
+++ unidade3/receita2/src/pages/index.js
@@ -2,12 +2,24 @@ import { home } from "./components/home";
 import { login } from "./components/login";
 import { navaBar } from "./components/navaBar";
 export default function index() {
-
     return (
-        <section>
-            {navaBar()}
+        <><section>
+            {navaBar("home","login")}
+            <div>
             {home()}
             {login()}
-        </section>    
+            </div>
+        </section>
+        <style jsx>
+            {`
+            * {
+                margin: 0px;
+                padding: 0px;
+            }
+            div {
+                display: flex;
+            }
+            `}
+        </style></>
     )
 }
