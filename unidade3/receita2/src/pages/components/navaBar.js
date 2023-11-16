395d982eb67634d0acd26aac28625d511eb3d608
Tallys-Aureliano <tallysaureliano@outlook.com>
2023-11-15 21:29:11 -0300

    :sparkles:feat: Adding css and parameters

--- unidade3/receita2/src/pages/components/navaBar.js
+++ unidade3/receita2/src/pages/components/navaBar.js
@@ -1,11 +1,26 @@
-export function navaBar() {
+export function navaBar(link1, link2) {
     return (
-        <nav>
-            <h1>Receita 2</h1>
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/">Login</a></li>
-            </ul>
+        <><nav>
+            <h1>Componente Nav Bar</h1>
+            <a href="/">{link1}</a>
+            <a href="/">{link2}</a>
         </nav>
+        <style jsx>
+        {`
+            nav {
+                display: flex;
+                color: white;
+                justify-content: space-evenly;
+                align-items: center;
+                width: 100%;
+                background-color: black;
+                height: 100px;
+            }
+            a {
+                color: orange;
+
+            }
+        `}
+        </style></>
     )
 }
