951965a301480d884233d7fb032bfa5aa7326fcf
Tallys-Aureliano <tallysaureliano@outlook.com>
2023-11-22 18:29:28 -0300

    :sparkles:feat: Starting task

---
+++ unidade3/receita3/src/pages/movies.js
@@ -0,0 +1,22 @@
+export default function Movies({data}){
+    return (
+        <div>
+            <div>
+            {data.Search.map( (m) => <div>{m.Title} --- {m.Year}</div>  )}               
+            </div>
+        </div>
+    )
+}
+export async function getServerSideProps(context) {
+    const res = await fetch(`http://www.omdbapi.com/?apikey=${process.env.API_KEY}&s=bagdad`)
+    try {
+        const data = await res.json()
+        return {
+            props: {
+                data
+            }
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
