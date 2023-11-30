2716e03c7a2c47941fea24826f056e56bf72bf64
Tallys-Aureliano <tallysaureliano@outlook.com>
2023-11-30 14:00:38 -0300

    :sparkles:feat: Request on client and server

---
+++ unidade3/receita4/pages/components/movies.js
@@ -0,0 +1,49 @@
+import Link from 'next/link'
+export default function Movies({ data }) {
+    return (
+        <div>
+            <div>
+                <table border="2" cellPadding="10">
+                    <thead>
+                        <tr>
+                            <th>Título</th>
+                            <th>Ano</th>
+                            <th>Foto</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {dataMovies(data)}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
+export async function getServerSideProps(context) {
+    const res = await fetch(`http://www.omdbapi.com/?apikey=${process.env.NEXT_PUBLIC_API_KEY}&s=batman`)
+    try {
+        const data = await res.json()
+        return {
+            props: {
+                data
+            }
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+export function dataMovies(data) {
+    try {
+        return data.Search.map((m) =>
+            <tr>
+                <td>{m.Title}</td>
+                <td>{m.Year}</td>
+                <Link href={`/components/movie?id=${m.imdbID}`}>
+                    <td><img src={m.Poster} style={{ width: 100 }}></img></td>
+                </Link>
+            </tr>
+        )
+    } catch (error) {
+        <h1>Algo deu errado me Movie</h1>
+    }
+}
---
+++ unidade3/receita4/pages/components/movies2.js
@@ -0,0 +1,53 @@
+import useSWR from 'swr'
+import Link from 'next/link'
+
+export default function Movies2() {
+    const { data, error } = useSWR(`http://www.omdbapi.com/?apikey=${process.env.NEXT_PUBLIC_API_KEY}&s=batman`, fetcher)
+    if (error) return <div>falha na requisição...</div>
+    if (!data) return <div>carregando...</div>
+    return (
+        <div style={{ display: 'flex', gap: 50 }}>
+            <table border="8" cellPadding="10">
+                <thead>
+                    <tr>
+                        <th>Título</th>
+                        <th>Ano</th>
+                        <th>Foto</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {dataMovies(data)}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+export async function fetcher(url) {
+    const res = await fetch(url)
+    const json = await res.json()
+    return json
+}
+
+export function dataMovies(data) {
+    try {
+        return data.Search.map((m) =>
+            <tr key={m.imdbID}>
+                <td>{m.Title}</td>
+                <td>{m.Year}</td>
+                <td>
+                    <Link href={`/components/movie?id=${m.imdbID}`}>
+
+                        <img src={m.Poster} style={{ width: 100 }}></img>
+
+                    </Link>
+                </td>
+            </tr>
+        )
+    }
+    catch (error) {
+        alert(error)
+        return (
+            <tr><h1>Algo deu errado</h1></tr>
+        )
+    }
+}
