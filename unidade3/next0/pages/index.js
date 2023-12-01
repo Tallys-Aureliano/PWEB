1a0c7519bbe3e5345514a210111a4d832ce3fca9
Tallys-Aureliano <tallysaureliano@outlook.com>
2023-12-01 18:29:01 -0300

    :sparkles:Feat: Quadro de receitas

--- unidade3/next0/pages/index.js
+++ unidade3/next0/pages/index.js
@@ -1,12 +1,33 @@
-import { home } from "./modules/home";
-import { login } from "./modules/login";
+import Link from 'next/link'
 export default function index() {
 
     return (
         <div>
-            <h1>Bem vindo ao Next</h1>
-            {home()}
-            {login()}
+            <h1>Receitas por <a href="https://github.com/tallysdev/PWEB">Tallys Aureliano</a></h1>
+            <Link href="/receita0/receita0">
+                <h1>Receita 0 - 1</h1>
+                {/* <h2><a>Codigo</a></h2> */}
+            </Link>
+            <Link href="/receita2/receita2">
+                <h1>Receita2</h1>
+                {/* <h2><a>Codigo</a></h2> */}
+            </Link>
+            <Link href="/receita3/receita3">
+                <h1>Receita3</h1>
+                {/* <h2><a>Codigo</a></h2> */}
+            </Link>
+            <Link href="/receita4/receita4">
+                <h1>Receita4</h1>
+                {/* <h2><a>Codigo</a></h2> */}
+            </Link>
+            <Link href="/receita5/receita5">
+                <h1>Receita5</h1>
+                {/* <h2><a>Codigo</a></h2> */}
+            </Link>
+            <Link href="/receita6/receita6">
+                <h1>Receita6</h1>
+                {/* <h2><a>Codigo</a></h2> */}
+            </Link>
         </div>
     )
 }
