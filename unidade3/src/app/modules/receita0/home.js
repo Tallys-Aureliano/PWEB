c9328295cf5428994700e8af949e415894f0c417
Tallys-Aureliano <tallysaureliano@outlook.com>
2023-12-04 20:43:47 -0300

    rename past in new projetc

---
+++ unidade3/src/app/modules/receita0/home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+export default function Home() {
+    return (
+        <div>
+            <h2>Componente Home</h2>
+            <p> Faço parte do Home</p>
+        </div>
+    )
+}
---
+++ unidade3/src/app/modules/receita0/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Home from "./home"
+import Login from "./login"
+const Index = () => {
+
+    return (
+        <div>
+            <h1>Bem vindo ao Next</h1>
+            <Home />
+            <Login />
+        </div>
+    )
+}
+export default Index
